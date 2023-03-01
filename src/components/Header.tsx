0cbe9b704a9a17bbf444560cdf58a3da1123134b
jzlbornoz <javieraap10@gmail.com>
2023-02-28 20:02:29 -0400

    [icon menu]

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,16 @@ import React from 'react'
 import Logo from '../assets/logo.png'
 import styles from '../styles/components/Header.module.css'
 
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
 const Header = () => {
   return (
     <header className={styles.Header}>
         <Image src={Logo} alt='Logo' width={60} height={60}/>
-        <p>menu</p>
+        <FontAwesomeIcon icon={faBars} width={40} />
     </header>
   )
 }
