274bff5a38a87eddbc00b9dea94c869911c2c11b
jzlbornoz <javieraap10@gmail.com>
2023-03-07 19:48:57 -0400

    [landing page fixed]

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,6 +3,7 @@ import style from '../styles/components/LandingPage.module.css';
 import Messi from '../assets/messi-chest.jpg';
 import Cr7 from '../assets/cr7.jpg';
 import Modric from '../assets/modric.jpeg';
+import Italy from '../assets/italy.webp';
 import Logo from '../assets/logo.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -38,6 +39,10 @@ const LandingPage = () => {
                     <FontAwesomeIcon icon={faBoxesPacking} width={140} />
                 </div>
             </section>
+            <section className={style['Home-Frase']}>
+                <p>No necesitas ser un profesional para tener alta gama</p>
+                <button type='button'>Adquiere tu Jersey</button>
+            </section>
             <section className={style['Home-Pedidos']}>
                 <Image src={Modric} alt='Modric image' width={1200} />
                 <div>
@@ -48,25 +53,24 @@ const LandingPage = () => {
                     <FontAwesomeIcon icon={faFileCircleCheck} width={140} />
                 </div>
             </section>
-            <section className={style['Home-Frase']}>
-                <p>No necesitas ser un profesional para tener alta gama</p>
-                <button type='button'>Adquiere tu Jersey</button>
-            </section>
             <section className={style['Home-About']}>
-                <h2>Acerca de
-                    nosotros</h2>
-                <p>
-                    Toty sports es un proyecto único dedicado a la distribución legítima de nuestros productos enfocados
-                    en el área deportiva. Nos basamos desde la inspiración de los protagonistas, hasta cada aspecto que
-                    nos representa y une por la misma pasión que nos rodea a todos, cada símbolo, color y expresión han sido
-                    significativos desde nuestras raíces. Toty sports promoverá  ropa deportiva
-                    de alta calidad y gusto para jóvenes
-                    y adultos. La tienda es de tipo unipersonal y contamos con una firma propia de alta calidad que
-                    queremos que penetre fuertemente en el mercado con el nombre de marcas prestigiosas, así mismo,
-                    con marcas y modelos única y exclusivamente con el nombre de nuestra empresa. La diversidad
-                    de productos de todo tipo de peculiaridades deportivas,
-                    hace que todo deporte tenga su “rinconcito”en nuestra tienda.
-                </p>
+                <Image src={Italy} alt='Italy image' width={2200} />
+                <div>
+                    <h2>Acerca de
+                        nosotros</h2>
+                    <p>
+                        Toty sports es un proyecto único dedicado a la distribución legítima de nuestros productos enfocados
+                        en el área deportiva. Nos basamos desde la inspiración de los protagonistas, hasta cada aspecto que
+                        nos representa y une por la misma pasión que nos rodea a todos, cada símbolo, color y expresión han sido
+                        significativos desde nuestras raíces. Toty sports promoverá  ropa deportiva
+                        de alta calidad y gusto para jóvenes
+                        y adultos. La tienda es de tipo unipersonal y contamos con una firma propia de alta calidad que
+                        queremos que penetre fuertemente en el mercado con el nombre de marcas prestigiosas, así mismo,
+                        con marcas y modelos única y exclusivamente con el nombre de nuestra empresa. La diversidad
+                        de productos de todo tipo de peculiaridades deportivas,
+                        hace que todo deporte tenga su “rinconcito”en nuestra tienda.
+                    </p>
+                </div>
             </section>
         </section>
     )
