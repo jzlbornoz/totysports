3d90bcb49a2259ffa51beedff3393b259af0526a
jzlbornoz <javieraap10@gmail.com>
2023-05-12 19:52:20 -0400

    [store & jerseycard add tailwind]

--- src/components/client/JerseyCard.tsx
+++ src/components/client/JerseyCard.tsx
@@ -1,47 +1,69 @@
 'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useContext } from 'react'
+import Link from 'next/link'
 
 import { JerseyModel } from '@/models/jersey.model'
-
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlane, faPlaneCircleCheck } from '@fortawesome/free-solid-svg-icons';
-
-import Logo from '../../assets/logo.png'
-import style from '../../styles/components/JerseyCard.module.css'
+import { AppContext } from '@/context/AppContex'
 
 const JerseyCard = ({ jersey }: { jersey: JerseyModel }) => {
+    const { addToCart } = useContext(AppContext)
     return (
-        <div className={style.JerseyCard}>
-            <section className={style["JerseyCard-ImageSection"]}>
-                {
-                    jersey.img
-                        ? <Image src={jersey.img[0]} alt="f" width={250} height={250} />
-                        : <Image src={Logo} alt="e" width={1200} height={1200} />
-                }
-            </section>
-            <section className={style["JerseyCard-ContentSection"]}>
-                <p>
-                    {jersey.name.length > 37
-                        ? `${jersey.name.slice(0, 36).trimEnd().toUpperCase()}...`
-                        : jersey.name.toUpperCase()}
-                </p>
-                <div>
-                    <p style={jersey.sale > 0 ? { "textDecoration": 'line-through' } : { "color": '#9BFD5C' }}>{jersey.price}$</p>
-                    <p>
-                        {jersey.arrive
-                            ? <FontAwesomeIcon icon={faPlaneCircleCheck} />
-                            : <FontAwesomeIcon icon={faPlane} />}
-                    </p>
-                </div>
-            </section>
-            {jersey.sale > 0
-                ? <div className={style["JerseyCard-SaleTag"]}>
-                    <p>{jersey.sale}$</p>
-                </div>
-                : <></>}
+        <div className="group relative block overflow-hidden">
+            <button
+                className="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75"
+            >
+                <span className="sr-only">Wishlist</span>
+
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-4 w-4"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                    />
+                </svg>
+            </button>
+            <Link href={`/store/${jersey.id}`}>
+                <Image
+                    src={jersey.img[0]}
+                    alt=""
+                    width={1200}
+                    height={1200}
+                    className="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+                />
+            </Link>
+
+
+            <div className="relative border border-gray-100 bg-white p-6">
+                <span
+                    className="whitespace-nowrap bg-primary px-3 py-1.5 text-xs font-medium"
+                >
+                    {jersey.season}
+                </span>
+
+                <h3 className="mt-4 text-lg font-medium text-gray-900 truncate">{jersey.name}</h3>
+
+                <p className="mt-1.5 text-sm text-gray-700">{jersey.sale}$</p>
+
+                <form className="mt-4">
+                    <button
+                        className="block w-full rounded bg-primary p-4 text-sm font-medium transition hover:scale-105"
+                        onClick={() => addToCart(jersey)}
+                        type='button'
+                    >
+                        Add to Cart
+                    </button>
+                </form>
+            </div>
         </div>
+
     )
 }
 
--- src/components/client/Store.tsx
+++ src/components/client/Store.tsx
@@ -2,12 +2,10 @@
 
 import { JerseyModel } from '@/models/jersey.model'
 import React from 'react'
-
-import styles from '../../styles/components/Store.module.css'
 import { JerseyCard } from './JerseyCard'
 
 import { motion } from 'framer-motion';
-import Link from 'next/link'
+
 
 const Store = ({ jerseys }: { jerseys: JerseyModel[] }) => {
     const container = {
@@ -25,24 +23,35 @@ const Store = ({ jerseys }: { jerseys: JerseyModel[] }) => {
         show: { opacity: 1, scale: 1, },
     }
     return (
-        <main className={styles.Store}>
-            <h2>Oficial Jerseys</h2>
-            <motion.section className={styles['Store-Wrapped']}
-                variants={container}
-                initial="hidden"
-                animate="show"
-            >
-                {jerseys.map(item => (
-                    <motion.div key={item.id}
-                        variants={itemMotion}
-                    >
-                        <Link href={`/store/${item.id}`}>
-                        <JerseyCard jersey={item} />
-                        </Link>    
-                    </motion.div>
-                ))}
-            </motion.section>
-        </main>
+        <section>
+            <div className="max-w-screen-2xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8 mt-[11vh] bg-white">
+                <header>
+                    <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+                        Product Collection
+                    </h2>
+
+                    <p className="max-w-md mt-4 text-gray-500">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+                        praesentium cumque iure dicta incidunt est ipsam, officia dolor fugit
+                        natus?
+                    </p>
+                </header>
+
+                <motion.ul className="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4"
+                    variants={container}
+                    initial="hidden"
+                    animate="show">
+                    {jerseys.map(item => (
+                        <motion.li key={item.id}
+                            variants={itemMotion}
+                        >
+                            <JerseyCard jersey={item} />
+                        </motion.li>
+                    ))}
+                </motion.ul>
+            </div>
+        </section>
+
     )
 }
 
