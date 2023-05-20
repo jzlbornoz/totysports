914e795c03caf6fad2315993bf0f86635d75980d
jzlbornoz <javieraap10@gmail.com>
2023-05-20 13:04:59 -0400

    [menu icons fixed]

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,7 +4,7 @@ import { AppContext } from '@/context/AppContex';
 import Link from 'next/link';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouseCircleCheck, faStore, faPiggyBank, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faHouseCircleCheck, faStore, faEarthAmerica, faCartShopping, faShieldHalved, faHeartCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
 import { motion } from "framer-motion";
 
@@ -45,52 +45,21 @@ const Menu = () => {
         >
             <div className="px-4 py-6">
                 <nav aria-label="Main Nav" className="mt-6 flex flex-col space-y-1">
-                    <a
-                        href="#"
-                        className="flex items-center gap-2 rounded-lg bg-original px-4 py-2 text-gray-700"
+                    <Link
+                        href="/store"
+                        className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 opacity-75"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                            />
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                            />
-                        </svg>
+                        <FontAwesomeIcon icon={faStore} />
 
                         <span className="text-sm font-medium"> Tienda </span>
-                    </a>
+                    </Link>
 
                     <details className="group [&_summary::-webkit-details-marker]:hidden">
                         <summary
                             className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                         >
                             <div className="flex items-center gap-2">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-5 w-5 opacity-75"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-                                    />
-                                </svg>
+                                <FontAwesomeIcon icon={faHouseCircleCheck} />
 
                                 <span className="text-sm font-medium">Stock</span>
                             </div>
@@ -112,95 +81,45 @@ const Menu = () => {
                         </summary>
 
                         <nav aria-label="Teams Nav" className="mt-2 flex flex-col px-4">
-                            <a
+                            <Link
                                 href="#"
                                 className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-5 w-5 opacity-75"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-                                    />
-                                </svg>
+                                <FontAwesomeIcon icon={faEarthAmerica} />
 
                                 <span className="text-sm font-medium"> Naciones </span>
-                            </a>
+                            </Link>
 
-                            <a
+                            <Link
                                 href="#"
                                 className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-5 w-5 opacity-75"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                                    />
-                                </svg>
+                                <FontAwesomeIcon icon={faShieldHalved} />
+
 
                                 <span className="text-sm font-medium"> Equipos </span>
-                            </a>
+                            </Link>
                         </nav>
                     </details>
 
-                    <a
-                        href="#"
+                    <Link
+                        href="/cart"
                         className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 opacity-75"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-                            />
-                        </svg>
+                        <FontAwesomeIcon icon={faCartShopping} />
+
 
                         <span className="text-sm font-medium"> Carro de compras </span>
-                    </a>
+                    </Link>
 
-                    <a
-                        href="#"
+                    <Link
+                        href="favorites"
                         className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 opacity-75"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"
-                            />
-                        </svg>
+                        <FontAwesomeIcon icon={faHeartCircleCheck} />
 
                         <span className="text-sm font-medium"> Favoritos </span>
-                    </a>
+                    </Link>
 
                     <details className="group [&_summary::-webkit-details-marker]:hidden">
                         <summary
@@ -242,7 +161,7 @@ const Menu = () => {
                         </summary>
 
                         <nav aria-label="Account Nav" className="mt-2 flex flex-col px-4">
-                            <a
+                            <Link
                                 href="#"
                                 className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                             >
@@ -262,9 +181,9 @@ const Menu = () => {
                                 </svg>
 
                                 <span className="text-sm font-medium"> Details </span>
-                            </a>
+                            </Link>
 
-                            <a
+                            <Link
                                 href="#"
                                 className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                             >
@@ -284,7 +203,7 @@ const Menu = () => {
                                 </svg>
 
                                 <span className="text-sm font-medium"> Security </span>
-                            </a>
+                            </Link>
 
                             <form action="/logout">
                                 <button
